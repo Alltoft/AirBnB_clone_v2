66991c8e485dfa50685dda0ceca362900e031fa4
Emran <ouakouriimran@gmail.com>
2024-02-16 16:44:55 +0100

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """a Methodthat writes HBNB on the web"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display():
+    """a Methodthat writes HBNB message"""
+    return "HBNB"
+
+
+@app.route("/c/<name>", strict_slashes=False)
+def c(name):
+    """a Methodthat writes C + {name} message"""
+    new_name = name.replace("_", " ")
+    return "C {}".format(new_name)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """a Methodthat writes python + {text} message"""
+    new_text = text.replace("_", " ")
+    return "Python {}".format(new_text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
