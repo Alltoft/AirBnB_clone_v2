82de48aefb5690fc321cff7956695d17e419de03
Emran <ouakouriimran@gmail.com>
2024-02-16 16:12:54 +0100

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """a Methodthat writes HBNB on the web"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display():
+    """a Methodthat writes HBNB message"""
+    return "HBNB"
+
+
+@app.route("/c/<name>", strict_slashes=False)
+def c(name):
+    """a Methodthat writes C + {name} message"""
+    new_name = name.replace("_", " ")
+    return "C {}".format(new_name)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
